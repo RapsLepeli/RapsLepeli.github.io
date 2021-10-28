8ef2fcbf55b4dd03ece875af722ae5a503353393
Rapelang Lepeli <rapelanglepeli2000@gmail.com>
2021-10-28 21:22:25 +0200

    Git Repos

---
+++ scripts/git.js
@@ -0,0 +1,29 @@
+user = 'tik9'
+        apirepo = `https://api.github.com/users/${user}`
+        
+   listrepos = document.createElement('ul')
+        document.getElementById('github').appendChild(listrepos)
+        $.getJSON(apirepo + '/repos', function (data) {
+            console.log('data now', data)
+
+            function compare(a, b) {
+                if (a.watchers > b.watchers) {
+                    return -1
+                }
+                if (a.watchers < b.watchers) {
+                    return 1
+                }
+                return 0
+            }
+
+            data.sort(compare)
+            data.forEach(v => {
+
+                listItemRepo = document.createElement('li')
+                listrepos.appendChild(listItemRepo)
+                hlink = document.createElement('a')
+                listItemRepo.appendChild(hlink)
+                hlink.textContent = `${v.description} | Stars: ${v.watchers}`
+                hlink.href = v.html_url
+            })
+        })
